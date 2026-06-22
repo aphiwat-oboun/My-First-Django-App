40aa7a3579f8ec88bcff1264d5cb06adf24062f0
aphiwat-oboun <flim0617459683@gmail.com>
2026-06-22 10:52:21 +0700

    Config For Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+echo "Building project..."
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
 # Install dependencies
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
+
+# Create static directory to ensure Vercel doesn't throw a missing folder error
+mkdir -p staticfiles_build/static
 
 # Collect static files
 python3 manage.py collectstatic --noinput --clear
